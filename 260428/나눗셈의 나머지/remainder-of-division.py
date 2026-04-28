665c6e581b6db0aeb454914100b2893fb11700a5
KJM0603 <zing6587@gmail.com>
2026-04-28 19:22:14 +0900

    Title: 나눗셈의 나머지, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/나눗셈의 나머지/remainder-of-division.py
+++ 260428/나눗셈의 나머지/remainder-of-division.py
@@ -1,11 +1,10 @@
 A,B = map(int,input().split())
 
 ans_list = [0]*B
-while A >= B:
+while A > 1:
     w = A%B
     ans_list[w] += 1
     A //= B
-ans_list[A] += 1
 squared = list(map(lambda x: x**2, ans_list))
 ans = sum(squared)
 print(ans)
