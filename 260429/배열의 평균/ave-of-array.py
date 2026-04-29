a45aecb6f12cade82cfb2b9cbc39e1296998d761
KJM0603 <zing6587@gmail.com>
2026-04-29 11:55:47 +0900

    Title: 배열의 평균, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/배열의 평균/ave-of-array.py
+++ 260429/배열의 평균/ave-of-array.py
@@ -3,14 +3,14 @@ arr = [list(map(int,input().split())) for _ in range(2)]
 arr_t = [list(x) for x in zip(*arr)]
 
 for i in range(2):
-    print(sum(arr[i])/4, end=' ')
+    print(f'{sum(arr[i])/4:.1f}', end=' ')
 print()
 for j in range(4):
-    print(sum(arr_t[j])/2, end=' ')
+    print(f'{sum(arr_t[j])/2:.1f}', end=' ')
 print()
 total = 0
 for i in range(2):
     for j in range(4):
         total += arr[i][j]
 
-print(total/8)
+print(f'{total/8:.1f}')
